e448ad534838e0bb63083932d797ce2efcf6603c
unknown <greshje.nachc@gmail.com>
2021-11-27 20:48:27 -0500

    Delted unused line.

--- win/start-anonlink-entity-service.bat
+++ win/start-anonlink-entity-service.bat
@@ -5,5 +5,5 @@ echo Starting anonlink entity service (aes)...
 echo.
 echo.
 docker-compose -p anonlink -f ../tools/docker-compose.yml up --remove-orphans
-:: docker-compose -p anonlink -f ../anonlink-entity-service/tools/docker-compose.yml up --scale backend=8 --remove-orphans
+
 
