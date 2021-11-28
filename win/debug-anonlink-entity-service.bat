ce650eb343ebc273d7187c2fb417e9acdb00dd00
unknown <greshje.nachc@gmail.com>
2021-11-27 20:38:54 -0500

    Deleted unused file.

--- win/debug-anonlink-entity-service.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo.
-echo.
-echo Starting anonlink entity service (aes)...
-echo.
-echo.
-docker-compose -p anonlink -f ../tools/docker-compose-debug.yml up --remove-orphans
-:: docker-compose -p anonlink -f ../anonlink-entity-service/tools/docker-compose.yml up --scale backend=8 --remove-orphans
-
