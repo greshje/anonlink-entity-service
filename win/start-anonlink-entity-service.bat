9ea03c5a9943d8bc1ff955583f4b0c2489750e94
NACHC-CAD <greshje.nachc@gmail.com>
2021-12-27 18:18:51 -0500

    Work on creating LRP log file (bat file almost working, might need to rename to .sh)

--- win/start-anonlink-entity-service.bat
+++ win/start-anonlink-entity-service.bat
@@ -1,14 +1,40 @@
 @echo off
 echo.
 echo.
+:: STOP CONTAINERS
+echo Stopping all Containers...
+docker kill $(docker ps -q)
+FOR /f "tokens=*" %%i IN ('docker ps -aq') DO docker kill %%i
+:: DELETE CONTAINERS
 echo Deleting Containers...
 FOR /f "tokens=*" %%i IN ('docker ps -aq') DO docker rm %%i
 echo.
+:: PRUNE VOLUMES
 echo Pruning orphaned volumes
 docker volume prune -f
 echo.
+:: DELETE OLD FULL LOG FILE
+echo Deleting old full log file..
+touch full-log.txt
+del full-log.txt
+touch full-log.txt
+:: DELETE THE LRP LOG FILE
+echo Deleting old lrp log file..
+touch lrp-log.txt
+del lrp-log.txt
+touch lrp-log.txt
+:: TAIL THE LOG FILE (display the running process in a cygwin window)
+cygstart tail -f full-log.txt
+cygstart tail -f lrp-log.txt
+:: WRITE LOG_FILE LINES TO THE LRP LOG_FILE
+cygstart tail -f full-log.txt | grep "LOG_FILE:" > lrp-log.txt
+:: START AES
 echo Starting anonlink entity service (aes)...
-docker-compose -p anonlink -f ../tools/docker-compose.yml up --remove-orphans
+echo Process is running and writing log to ./full-log.txt
+echo Long Running Process Log (LRP) is being written to lrp-log.txt
+echo ! ! ! DO NOT CLOSE THIS WINDOW ! ! !
+echo (<ctrl-c> to quit the process)
+docker-compose -p anonlink -f ../tools/docker-compose.yml up --remove-orphans > full-log.txt
 echo.
 echo.
 echo Done.
